3ac69c930fa4905ad4179d4e16e3f75dc61eb933
Adrian Anton <antonadrian@gmail.com>
2015-12-31 12:57:10 +0200

    V002 - Clean up
    Am sters fisiere db back-up

--- config/settings1.inc.php
+++ config/settings1.inc.php
@@ -1,14 +1,14 @@
 <?php
 define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'lulumade_edenstage');
+define('_DB_NAME_', 'lulumade_lulustage');
 define('_DB_USER_', 'lulumade_admin');
 define('_DB_PASSWD_', 'Gigilica27');
-define('_DB_PREFIX_', 'ps_');
+define('_DB_PREFIX_', 'el_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
 define('_PS_CACHE_ENABLED_', '0');
-define('_COOKIE_KEY_', 'CoNZR62ddrfvkvHP5UJmVwK8Ly0dFk9jkKT6FbewxKEHU1PxnQu9CSxn');
-define('_COOKIE_IV_', 'PAR4UZbH');
+define('_COOKIE_KEY_', 'azHykswejwib2BovAmmQp6K2L41ViRUM3uHbH5iqFwZSWjcdCaqLeCgz');
+define('_COOKIE_IV_', '9JowWs3o');
 define('_PS_CREATION_DATE_', '2015-12-04');
 if (!defined('_PS_VERSION_'))
 	define('_PS_VERSION_', '1.6.1.3');
